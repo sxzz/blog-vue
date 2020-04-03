ecbb55e7742333d0cf890f5c733bd3bcfde95480
Kevin <sxzz@sxzz.moe>
2020-04-04 05:32:32 +0800

    fix: bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script>
-import Vue from "vue";
-import bContainer from "bootstrap-vue/es/components/layout/container";
-import "bootstrap/dist/css/bootstrap.css";
-
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-Vue.component("b-container", bContainer);
 
 export default {
   components: {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-
 import router from "./router";
 import api from "./api";
 
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
